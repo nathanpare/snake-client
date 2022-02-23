c07f2f2d2b5c46760394aa72d96bf826721009a4
Nathan Pare <nathanparebusiness@gmail.com>
2022-02-23 14:49:12 -0800

    import client side code from client to play

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "127.0.0.1",
+    port: 50541
+  });
+  console.log("Connecting ...");
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    
+    conn.end();
+  });
+  conn.on('end', () => {
+    console.log('disconnected from server');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+};
+
+
+connect();
+
+module.exports = {
+  connect,
+};
